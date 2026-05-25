1a5fd9ca8ea0c94a832d5bca8e5b87e1d55c7e47
Abdelrhman Yaseen <abedrahmanyassen@gmail.com>
2026-05-25 14:02:23 +0300

    deployment changes https://clinic-scheduling-agent.onrender.com

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     Electricity_Off: bool = False
     OLLAMA_MODEL: str = "command-r7b-arabic:latest"
     MONGODB_URL: str 
-    DATABASE_NAME: str = "clinic_bot"
+    DATABASE_NAME: str 
     GEMINI_MODEL_NAME: str = "gemini-2.5-flash-lite"
     MODEL_PROVIDER: str = "Fanar"  # or "Gemini"
     GEMINI_API_KEY: str 
