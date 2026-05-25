17d3dd66025295b21ec8a16e0c53001d8b772bdc
Abdelrhman Yaseen <abedrahmanyassen@gmail.com>
2026-05-25 13:34:56 +0300

    Mongo env

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Haven"
     Electricity_Off: bool = False
     OLLAMA_MODEL: str = "command-r7b-arabic:latest"
-    MONGODB_URL: str = "mongodb://localhost:27017"
+    MONGODB_URL: str 
     DATABASE_NAME: str = "clinic_bot"
     GEMINI_MODEL_NAME: str = "gemini-2.5-flash-lite"
     MODEL_PROVIDER: str = "Fanar"  # or "Gemini"
